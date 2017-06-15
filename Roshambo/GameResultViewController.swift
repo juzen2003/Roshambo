ebfa780c85cae5e65199adc5f76817a519bc8bf7
Yu-Jen Chang <juzen2003@gmail.com>
2017-06-15 01:53:18 -0700

    0.0.0.2 - complete storyboard for both GameResultVC & PlayGameVC, also complete the code the generate game result and send from PlayGameVC to GameResultVC

--- Roshambo/GameResultViewController.swift
+++ Roshambo/GameResultViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class GameResultViewController: UIViewController {
 
+    var resultImageName: String?
+    var resultMessage: String?
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        playAgainButton.setTitle("Play Again!", for: .normal)
+        playAgainButton.setTitleColor(UIColor.blue, for: .normal)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if let resultImageName = self.resultImageName {
+            self.resultImage.image = UIImage(named: resultImageName)
+        } else {
+            self.resultImage.image = nil
+        }
+        
+        if let resultMessage = self.resultMessage {
+            self.resultLabel.text = resultMessage
+            self.resultLabel.textColor = UIColor.blue
+        } else {
+            self.resultLabel.text = nil
+        }
+        
+        //self.resultImage.alpha = 0
     }
 
+    @IBAction func playAgain(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
--- Roshambo/PlayGameViewController.swift
+++ Roshambo/PlayGameViewController.swift
@@ -15,11 +15,49 @@ class PlayGameViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    enum theMove: Int {
+        case rock = 1, paper, scissors
+    }
+    
+    func randomInt() -> Int {
+        let randomValue = arc4random() % 3 + 1
+        return Int(randomValue)
+    }
+    
+    // generate the game result
+    func playGame(_ playerChoice: Int) -> (String, String) {
+        
+        let playerMove = theMove.init(rawValue: playerChoice)!
+        let computerMove = theMove.init(rawValue: randomInt())!
+        let imageName: String
+        let result: String
+        
+        switch (playerMove, computerMove) {
+        case (.rock, .paper), (.paper, .rock):
+            result = "Paper covers Rock!"
+            imageName = "PaperCoversRock"
+        case (.rock, .rock), (.paper, .paper), (.scissors, .scissors):
+            result = "It's a Tie!"
+            imageName = "itsATie"
+        case (.rock, .scissors), (.scissors, .rock):
+            result = "Rock crushes Scissors!"
+            imageName = "RockCrushesScissors"
+        case (.paper, .scissors), (.scissors, .paper):
+            result = "Scissors cut Paper!"
+            imageName = "ScissorsCutPaper"
+        }
+        
+        return (result, imageName)
+    }
+    
+    // Code only for Rock move
     @IBAction func performRock() {
         var controller: GameResultViewController
         
         controller = self.storyboard?.instantiateViewController(withIdentifier: "GameResultViewController") as! GameResultViewController
-        
+        let play = playGame(1)
+        controller.resultImageName = play.1
+        controller.resultMessage = play.0
         
         present(controller, animated: true, completion: nil)
     }
